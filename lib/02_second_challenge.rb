b38444a501891d27f5f4f9859c4685f07245a083
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-24 16:59:12 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    montague => {
-      patriarch => { },
-      matriach => { },
-      hero => { },
-      hero_friends => []
+    :montague => {
+      :patriarch => { },
+      :matriach => { },
+      :hero => { },
+      :hero_friends => []
       }, 
-    capulet => {
-      patriarch => { },
-      matriach => { },
-      hero => { },
-      hero_friends => []
+    :capulet => {
+      :patriarch => { },
+      :matriach => { },
+      :hero => { },
+      :hero_friends => []
       }
   }
   
