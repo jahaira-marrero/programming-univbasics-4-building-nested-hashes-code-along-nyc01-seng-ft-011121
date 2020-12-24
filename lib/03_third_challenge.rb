da72e60b68dccbb9922e3b02402f7a8407bde195
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-24 16:43:29 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,10 +1,7 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        :name "Lord Montague", :age "53"
-        },
+      :patriarch => {:name "Lord Montague", :age "53"},
       :matriarch => {
         #your key/value pairs here
         :name => "Lady Montague", :age => "54"
